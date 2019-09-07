6cd5032411de4734e6bfafe80a9a683fb8628564
Diksha65 <agarwaldiksha6596@gmail.com>
2019-09-07 17:34:06 +0530

    Inital Commit

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ server.get('/hello/:name', (req, res) => {
     res.status(200).json({'hello' : req.params.name});
 });
 
-const port = 8000 || process.env.PORT;
+const port = process.env.PORT || 8000;
 server.listen(port, () => console.log('Ready.'));
